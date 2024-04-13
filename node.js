6d261017a8aaba9c2eea7ce34d8d70c10012c15a
Jason23020013 <163218805+Jason23020013@users.noreply.github.com>
2024-04-13 15:34:12 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,3 +1,12 @@
-npm install express body-parser node-fetch cors
+npm install @google/generative-ai
 
-npm init
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(AIzaSyDL_dJzWHhT3pKT8E17N8Kb1JfaBm_IU5c);
+
+// ...
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+// ...
